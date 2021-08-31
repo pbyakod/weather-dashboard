cc66e8fa6dc93e942788e80d93fe01b6d9a1b358
Pranav Byakod <pranav.byakod@gmail.com>
2021-08-31 10:21:54 -0700

    finding the city based on lat and lon and retrieving the weather

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,41 @@ function readableCity(enteredCityEl) {
         readableName[position] += readableName[position].slice(1);
         position = position + 1;
     }
-    var cityName = readableName.join(" ");
-    return cityName.join(" ");
+    var readable = readableName.join(" ");
+    return readable.join(" ");
+}
+
+// using the city name to find its respective latitude and longitude
+var findLatLon = function(readable, apiKey) {
+    var apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${readable}&units=imperial&appid=${apiKey}`;
+    fetch(apiLink)
+    .then(function(response) {
+        if (response.ok) {
+            response.json()
+            .then(function(data) {
+                var longitude = data.coord['lon'];
+                var latitude = data.coord['lat'];
+                getWeather(longitude, latitude, apiKey);
+            })
+            .catch(function(err) {
+                alert("ERROR!", err);
+            })
+        }
+    });
+}
+
+// retrieving the weather data off the coordinated
+var getWeather= function(readable, longitude, latitude, apiKey) {
+    var apiCoordLink = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&exclude=minutely,hourly,alerts&appid=${apiKey}`;
+    fetch(apiCoordLink)
+    .then((response) => {
+        if(response.ok) {
+            return response.json();
+        } else {
+            console.log("ERROR! response not fetched")
+        }
+    })
+    .then((data) => {
+        readableEl.textContent = `${readable} ${moment().format("M/D/YYYY")}`; 
+    })
 }
